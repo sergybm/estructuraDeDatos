4c30c2105542751f6a81f8f3a93bc2fd2a4e94bc
raulruval <raulruizvalle@gmail.com>
2017-10-31 11:31:56 +0100

    P2 v1
    Supuestamente tiene que haber 1 cola con prioridad que es la de los clientes, genera los 100 clientes diferentes con todas sus cosas, ahora esos 100 clientes tienen que pasar por las cajas.
    
    Debe haber 10 cajas, no sé si hay otra forma de crear 10 cajas, pero bueno, lo he hecho a lo bruto, ahora con el metodo del CompareTo en caja, se deberian comparar las 10 cajas, y dar como resultado la caja a la que tiene que ir el próximo cliente.
    
    Lo de los abuelos creo que es simplemente, en la cola con prioridad de 100 personas que si hay un abuelo en ese vector se cuele y se ponga el primero para luego entrar a la caja que le corresponda.
    
    Estoy viendo ahora como hacer ese método CompareTo, el que hay puesto lo he sacado de internet pero no está bien puesto para nuestro problema.

--- Ejercicio2/Ejercicio2.java
+++ Ejercicio2/Ejercicio2.java
@@ -8,25 +8,34 @@ public class Ejercicio2 {
 	public static void main(String args[]) {
 		
 		int tiempoTranscurrido = 0; // Este será el tiempo que transcurre	
-		PriorityQueue<Caja> colacajas=new PriorityQueue<Caja>(); // La cola que se genera en las cajas.
-		Caja caja = new Caja(tiempoTranscurrido, tiempoTranscurrido); // Las cajas de las que disponemos.
+		LinkedBlockingQueue<Caja> colacaja0=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 0.
+		LinkedBlockingQueue<Caja> colacaja1=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 1.
+		LinkedBlockingQueue<Caja> colacaja2=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 2.
+		LinkedBlockingQueue<Caja> colacaja3=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 3.
+		LinkedBlockingQueue<Caja> colacaja4=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 4.
+		LinkedBlockingQueue<Caja> colacaja5=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 5.
+		LinkedBlockingQueue<Caja> colacaja6=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 6.
+		LinkedBlockingQueue<Caja> colacaja7=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 7.
+		LinkedBlockingQueue<Caja> colacaja8=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 8.
+		LinkedBlockingQueue<Caja> colacaja9=new LinkedBlockingQueue<Caja>(); // La cola que se genera en la caja 9.
+		
 		int sumadorTiempo = 1;
-		Queue<Cliente> colaclientes = new LinkedBlockingQueue<Cliente>(); // La cola de los clientes.
+		PriorityQueue<Cliente> colaclientes = new PriorityQueue<Cliente>(); // La cola de los clientes
 		Cliente cliente[] = new Cliente[100]; // Vector con todos los clientes.
 		
 		System.out.println("Por orden de llegada:\n"); // Imprime los clientes con sus productos y el tiempo en el que llegan.
 		for(int i=0; i<100; i++){
 			cliente[i] = new Cliente(tiempoTranscurrido); // Creamos el cliente y lo metemos en el array.
 			colaclientes.add(cliente[i]); // Metemos cada cliente en la cola de los clientes
-	           tiempoTranscurrido=tiempoTranscurrido+30; // Los clientes entran cada 30 segundos.
+	           tiempoTranscurrido=tiempoTranscurrido+30; // Tiempo de llegada de los clientes cada 30 segundos.
 	            System.out.println("Cliente " + i +": " +cliente[i].toString());
 	           
 		}       
         System.out.println("\n\nPor orden de salida:\n");        
-			pasandoPorCaja(colaclientes, caja, sumadorTiempo, sumadorTiempo); // Cliente pasa por caja y sale el tiempo.
+			pasandoPorCaja(colaclientes, sumadorTiempo, sumadorTiempo); // Cliente pasa por caja y sale el tiempo.
 		} 
 
-	public static void pasandoPorCaja(Queue<Cliente> colaclientes,Caja caja,int sumadorTiempo, int tiempoTranscurrido) {
+	public static void pasandoPorCaja(Queue<Cliente> colaclientes,int sumadorTiempo, int tiempoTranscurrido) {
 		int tiempoAtendiendo = 0, tiempoProducto = 0;
 
 		while (!colaclientes.isEmpty()) {	
@@ -49,4 +58,3 @@ public class Ejercicio2 {
 		}
 	}
 }
-	
