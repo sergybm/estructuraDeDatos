952e4c65b920aa0d90aa9444f41c90713461fb97
raulruval <raulruizvalle@gmail.com>
2017-11-09 18:43:05 +0100

    P2 V5
    Sigo con ella, he avanzado bastante, a ver si mañana la tengo ya acabada.

--- Ejercicio2/Caja.java
+++ Ejercicio2/Caja.java
@@ -1,24 +1,21 @@
 import java.util.Deque;
-import java.util.Queue;
 import java.util.concurrent.LinkedBlockingDeque;
 
-
-
-public class Caja implements Comparable<Caja>{
-	private int ID;
-	private int tiempoTrabajado; /*Cada posicion es el tiempo en pasar todos los productos de un cliente */
+public class Caja implements Comparable<Caja> {
+	private int id;
+	private int tiempoTrabajado; 
 	Deque<Cliente> clientescaja = new LinkedBlockingDeque<Cliente>(); // Es una cola doble para poder colar luego a los viejecitos.
 
-	public Caja(int i) {
-		int ID = i;
+	public Caja(int id) {
+		this.id = id;
 	}
-	
+
 	public int getID() {
-		return ID;
+		return id;
 	}
 
 	public void setID(char iD) {
-		ID = iD;
+		id = iD;
 	}
 
 	public int getEficienciaDespacho() { // Se generará aleatoriamente (máximo 20 segundos/producto).
@@ -29,36 +26,36 @@ public class Caja implements Comparable<Caja>{
 		return this.tiempoTrabajado = tiempoTrabajado;
 	}
 
-	public void setTiempoTrabajo(int sumadorTiempo) { 
+	public void setTiempoTrabajo(int sumadorTiempo) {
 		tiempoTrabajado += sumadorTiempo;
 	}
-	
+
 	public void MeterClienteEnCola(Cliente c) {
 		clientescaja.add(c);
 	}
-	
-	public Deque<Cliente> getCola() {	// Para retornar la cola de clientes de la caja
+
+	public void SacarClienteDeCola(Cliente c) {
+		clientescaja.poll();
+	}
+
+	public Deque<Cliente> getCola() { // Para retornar la cola de clientes de la caja
 		return clientescaja;
 	}
-		
-	@Override	
-public int compareTo(Caja uno) {
+
+	@Override
+	public int compareTo(Caja uno) {
 		if (uno.getCola().size() < getCola().size()) {
-			return -1;
-		}
-		if (uno.getCola().size() > getCola().size() ) {
 			return 1;
+		}
+		if (uno.getCola().size() > getCola().size()) {
+			return -1;
 		}
 		return 0;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "Caja [ID=" + id + ", tiempoTrabajado=" + tiempoTrabajado + ", clientescaja=" + clientescaja + "]";
+	}
 
 }
-	
-
-
-
-
-
-
--- Ejercicio2/Ejercicio2.java
+++ Ejercicio2/Ejercicio2.java
@@ -1,120 +1,100 @@
 import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.LinkedBlockingQueue;
-// import java.util.Comparator;
 
 public class Ejercicio2 {
-	
-	public static PriorityQueue<Caja> CrearCajas(){ /*/ Crea la lista de cajas y mete las 10 cajas /*/
-		PriorityQueue<Caja> colacajas=new PriorityQueue<Caja>(); // /// Cola para meter las cajas
-	
-		for (int i=0; i<10;i++) {
+
+	public static PriorityQueue<Caja> CrearCajas() { /* / Crea la lista de cajas y mete las 10 cajas / */
+		PriorityQueue<Caja> colacajas = new PriorityQueue<Caja>(); // Cola para meter las cajas
+
+		for (int i = 0; i < 10; i++) {
 			colacajas.add(new Caja(i)); // Creamos las 10 cajas y la añadimos
 		}
 		return colacajas;
-		}
-	
+	}
 
-	public static Deque<Cliente> CrearClientes(){ 	/*/ Crea los clientes y los mete en una cola doble  /*/
+	public static Deque<Cliente> CrearClientes() { // Crea los clientes y los mete en una cola doble 
 		Deque<Cliente> colaclientes = new LinkedBlockingDeque<Cliente>();
 		int tiempoLlegada = 0;
-		for(int i=0; i<100; i++){ // La cola de los clientes iniciales, los 100 clientes.
-			colaclientes.add(new Cliente(tiempoLlegada)); // Metemos los clientes en la cola de clientes
-	        tiempoLlegada=tiempoLlegada+30; //El tiempo de llegada del proximo cliente será en 30 segundos.      
+		int contadorViejos=(int)Math.random()*(100-0+1)+100; //Valor entre 0 y 100, ambos incluidos. 
+		
+		for (int i=0;i<contadorViejos;i++) {
+		colaclientes.add(new ClienteViejo(tiempoLlegada)); // Añado los clientes viejos que se hayan generado aleatoriamente a la cola.
+		}
+		
+		for (int i = 0; i < (100-contadorViejos); i++) { // La cola de los clientes iniciales, los 100 clientes.
+			colaclientes.add(new Cliente(tiempoLlegada)); // Metemos los clientes en la cola de clientes.
 		}
 		return colaclientes;
 	}
-	
-	
-	
+
 	public static void MeterClientes() {
-		PriorityQueue<Caja> colacajas=CrearCajas(); /// Metemos en la cola colacajas las 10 cajas creadas;
-		Deque<Cliente> colaclientes = CrearClientes();    
 		
-		int tiempoTranscurrido = 0; // Este será el tiempo que transcurre	, cada 30 segundos entra un nuevo cliente
-	
-		for (int i = 0; i < colaclientes.size();i++) {
-			
+		PriorityQueue<Caja> colacajas = CrearCajas(); /// Metemos en la cola colacajas las 10 cajas creadas;
+		Deque<Cliente> colaclientes = CrearClientes(); // Cola doble de clientes
+		int size=colaclientes.size();
+		int tiempoTranscurrido = 0; // Este será el tiempo que transcurre , cada 30 segundos entra un nuevo cliente
+		int numClientesCadaCaja = 0;
+		int numProductosTotal = 0;
+		
+
+		for (int i = 0; i < size; i++) {
+
 			Cliente cli = colaclientes.poll(); // Saco cliente de la cola
-			Caja caja =colacajas.peek(); // Miro cual es la caja en la peek, que es en teoría, la caja con la cola mas pequeña y donde debe ir el cliente que queremos meter. 
-			caja=colacajas.poll();
+			Caja caja = colacajas.poll(); // Saco una caja de la cola de cajas, que es en teoría, la caja con la cola mas corta y donde debe ir el cliente que queremos meter.
 			
 			try {
+				// Thread.sleep(30000); // Espera 30 segundos para meter a un cliente nuevo
 				if (cli instanceof ClienteViejo) {
-					caja.getCola().addFirst(cli); // Meto el cliente viejo en la caja
-						tiempoTranscurrido=tiempoTranscurrido+30; // Al meter un cliente añadimos al tiempo transcurrido 30 segundos
-				}else {
-					tiempoTranscurrido=+30; // Al meter un cliente añadimos al tiempo transcurrido 30 segundos.
-					caja.getCola().add(cli);	// Meto el cliente normal en la caja.
-			}
-				
-				colacajas.add(caja); // Actualizamos la coja de cajas con la nueva información
-				
-		}catch(Exception ex) {
-			System.out.println("Error en la creación.");
-		}
-			
-		}
-
-	}
-	
-
-	
-	
-	
-	public static void main(String args[]) {
-		
-		System.out.println("Por orden de llegada:\n"); // Imprime los clientes con sus productos y el tiempo en el que llegan.
-		
-		 System.out.println(CrearClientes().toString()); 
-		 
-        System.out.println("Por orden de salida:\n");  
-        MeterClientes();
-        	// pasandoPorCaja(colaclientes, colacajas, sumadorTiempo, sumadorTiempo); // Cliente pasa por caja y sale el tiempo
-        }
-        
-		
-
-	
-	public static void pasandoPorCaja(Queue<Cliente> colaclientes,PriorityQueue<Caja> colacajas,int sumadorTiempo, int tiempoTranscurrido) {
-		int tiempoAtendiendo = 0, tiempoProducto = 0;
-
-		while (!colaclientes.isEmpty()) {	
-			Caja c;
-			
-		/*/	Cliente clienteEnCaja;
-			clienteEnCaja = colaclientes.poll();
-			int eficiencia = 0;
-			
-			for (int i = clienteEnCaja.getProductos(); i > 0; i--) {
-				while (tiempoProducto <= eficiencia) {
-					tiempoProducto += sumadorTiempo;
+					tiempoTranscurrido = tiempoTranscurrido + 30; // Al meter un cliente añadimos al tiempo transcurrido 30 segundos
+					cli.setTiempoLlegada(tiempoTranscurrido);
+					caja.MeterClienteEnCola(cli);
+					colacajas.add(caja); // Vuelvo a meter la caja a la cola de cajas.
+				 	
+					
+				} else {
+					tiempoTranscurrido = tiempoTranscurrido + 30; // Al meter un cliente añadimos al tiempo transcurrido 30 segundos.
+					cli.setTiempoLlegada(tiempoTranscurrido);
+					caja.MeterClienteEnCola(cli);
+					colacajas.add(caja); // Vuelvo a meter la caja a la cola de cajas.
 				}
-				tiempoAtendiendo += tiempoProducto;
-				tiempoProducto = 0;
-			} /*/
-			System.out.println("El tiempo transcurrido con el cliente ha sido de:" + tiempoAtendiendo);
-			tiempoAtendiendo = 0;
+				
+				
 
+			} catch (Exception ex) {
+				System.out.println("Error en la creación.");
+			}
 		}
+
+		
+			
+	while (!colacajas.isEmpty()) {
+				System.out.println("Caja " + colacajas.peek().getID() + " contiene:\n" + colacajas.peek().getCola()); // Imprimo la caja y los clientes que tiene
+			for(int j=0;j<numClientesCadaCaja;j++) {
+					numProductosTotal=numProductosTotal+colacajas.peek().getCola().poll().getProductos();  //Saco los productos totales de una caja 
+				}
+				int tiempoDespacho=numProductosTotal*(colacajas.peek().getEficienciaDespacho()); // Calculo el tiempo que tardo en despachar en la caja y se lo asigno.
+				colacajas.peek().setTiempoTrabajo(tiempoDespacho); 
+		
+				colacajas.poll();
+							
+		}
+
+		
+		} /// FIN DEL MÉTODO MeterClientes
+		
+	
+
+	
+
+	public static void main(String args[]) {
+
+		System.out.println("Los clientes llegan a la cola:\n"); // Imprime los clientes con sus productos y el tiempo en el que llegan.
+		System.out.println(CrearClientes().toString());
+
+		System.out.println("\nLos clientes entran a las cajas:\n");
+		MeterClientes();
+		
+		System.out.println("");
 	}
 
-	
-	/*/	static class colas implements Comparator<Caja>{ // Para comparar las cajas
-
-	@Override
-	public int compare(Caja uno, Caja dos) {
-		if (uno.getCola().size() < dos.getCola().size()) {
-			return -1;
-		}
-		if (uno.getCola().size() > dos.getCola().size() ) {
-			return 1;
-		}
-		return 0;
-	}
-	
-} /*/
-	
-	
 }
-	
